7df35992d94cd99075141fbd51048c4880bd089c
Joe97-dev <joekazungu@gmail.com>
2023-08-30 11:45:12 +0300

    added requirements.txt and setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version = '0.0.0',
+    author = 'Joe97_dev',
+    author_email = "joekazungu@gmail.com",
+    license="MIT",
+    packages = find_packages(),
+    install_requires = []
+)
